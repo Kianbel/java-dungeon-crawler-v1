b1526fdc325b360bfb490a752d40a96462a17dd6
Kianbel <kirkianbelcina@gmail.com>
2026-06-24 21:39:23 +0800

    todo change characters

--- src/main/java/item/armor/IronMail.java
+++ src/main/java/item/armor/IronMail.java
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color;
 public class IronMail extends Armor {
     public IronMail() {
         super("Iron Mail", 10);
-        overrideCharacter("🥼");
+        overrideCharacter("🥼"); // TODO: change character
         overrideColor(Color.WHITESMOKE);
     }
 }
--- src/main/java/item/heal/HealingPotion.java
+++ src/main/java/item/heal/HealingPotion.java
@@ -8,7 +8,7 @@ public class HealingPotion extends Potion {
     public HealingPotion(int amount) {
         super("Healing Potion", amount);
         healPoints = 10;
-        overrideCharacter("𐃯");
+        overrideCharacter("𐃯"); // TODO: change character
         overrideColor(UITheme.STAT_HEALTH);
     }
 }
